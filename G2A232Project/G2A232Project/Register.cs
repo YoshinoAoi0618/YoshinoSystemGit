07127b785cdfc52551cf8ebcd2d6498c552426fd
YoshinoAoi <20217018-YoshinoAoi@hcs.ac.jp>
2022-12-09 15:09:46 +0900

    Loadでデータグリッドの偶数行の背景色を変更 列名を中央揃えなど...を追加 23行目～30行目

--- G2A232Project/G2A232Project/Register.cs
+++ G2A232Project/G2A232Project/Register.cs
@@ -20,10 +20,57 @@ namespace G2A232Project
         private Menu _menu;
         private void Register_Load(object sender, EventArgs e)
         {
+            //偶数行の背景色を変更
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.SkyBlue;
+            //Gridの幅を列に揃える
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            //行の高さをヘッダーとセルに合わせて自動調整
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            //列の項目名を中央揃え
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
         }
         //登録
         private void button3_Click(object sender, EventArgs e)
         {
+            try
+            {
+                using (SQLiteConnection con = new SQLiteConnection("Data Source=G2A232.db"))
+                {
+                    con.Open();
+                    using (SQLiteTransaction trans = con.BeginTransaction())
+                    {
+                        SQLiteCommand cmd = con.CreateCommand();
+                        // インサート
+                        cmd.CommandText = "INSERT INTO t_product (name, address, birth, tel, email) VALUES (@Name, @Address, @Birth, @Tel, @Email)";
+                        // パラメータセット
+                        cmd.Parameters.Add("Name", System.Data.DbType.String);
+                        cmd.Parameters.Add("Address", System.Data.DbType.String);
+                        cmd.Parameters.Add("Birth", System.Data.DbType.Int64);
+                        cmd.Parameters.Add("Tel", System.Data.DbType.String);
+                        cmd.Parameters.Add("Email", System.Data.DbType.String);
+
+                        // データ追加
+                        cmd.Parameters["Name"].Value = textBox1.Text;
+                        cmd.Parameters["Address"].Value = textBox2.Text;
+                        cmd.Parameters["Birth"].Value = int.Parse(textBox3.Text);
+                        cmd.Parameters["Tel"].Value = textBox4.Text;
+                        cmd.Parameters["Email"].Value = textBox5.Text;
+                        cmd.ExecuteNonQuery();
+                        // コミット
+                        trans.Commit();
+                    }
+                }
+                textBox1.ResetText();
+                textBox2.ResetText();
+                textBox3.ResetText();
+                textBox4.ResetText();
+                textBox5.ResetText();
+            }
+            //条件に合わなかったら、メッセージボックスにエラー内容を表示
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
             
         }
         //戻る
@@ -31,12 +78,20 @@ namespace G2A232Project
         {
             _menu = new Menu();
             _menu.Show();
-            this.Enabled = false;
+            this.Visible = false;
         }
         //確認
         private void button1_Click(object sender, EventArgs e)
         {
-
+            using (SQLiteConnection con = new SQLiteConnection("Data Source=G2A232.db"))
+            {
+                // DataTableを生成します。
+                var dataTable = new DataTable();
+                // SQLの実行
+                var adapter = new SQLiteDataAdapter("SELECT * FROM t_product", con);
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
         }
     }
 }
