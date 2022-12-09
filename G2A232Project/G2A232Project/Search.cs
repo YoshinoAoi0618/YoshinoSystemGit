287d82b3bb19a4f56b9997712b76a940daf6a62c
YoshinoAoi <20217018-YoshinoAoi@hcs.ac.jp>
2022-12-09 15:06:30 +0900

    IDでデータを検索出来るように実行

--- G2A232Project/G2A232Project/Search.cs
+++ G2A232Project/G2A232Project/Search.cs
@@ -7,14 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace G2A232Project
 {
     public partial class Search : Form
     {
+        private Menu _menu;
         public Search()
         {
             InitializeComponent();
+        }
+        private void Search_Load(object sender, EventArgs e)
+        {
+            //偶数行の背景色を変更
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.SkyBlue;
+            //Gridの幅を列に揃える
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            //行の高さをヘッダーとセルに合わせて自動調整
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            //列の項目名を中央揃え
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        }
+
+        //検索
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SQLiteConnection con = new SQLiteConnection("Data Source=G2A232.db"))
+                {
+                    // DataTableを生成します。
+                    var dataTable = new DataTable();
+                    // SQLの実行
+                    var adapter = new SQLiteDataAdapter("SELECT * FROM t_product WHERE Id= "+ textBox6.Text, con);
+                    adapter.Fill(dataTable);
+                    dataGridView1.DataSource = dataTable;
+                }
+            }
+            //条件に合わなかったら、メッセージボックスにエラー内容を表示
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        //戻る
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _menu = new Menu();
+            _menu.Show();
+            this.Visible = false;
+        }
+        //確認
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SQLiteConnection con = new SQLiteConnection("Data Source=G2A232.db"))
+            {
+                // DataTableを生成します。
+                var dataTable = new DataTable();
+                // SQLの実行
+                var adapter = new SQLiteDataAdapter("SELECT * FROM t_product", con);
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
         }
     }
 }
