b5880810342de0dacdb804fee86ebc7ad9648b3c
YoshinoAoi <20217018-YoshinoAoi@hcs.ac.jp>
2022-12-21 11:25:12 +0900

    メソッド名の変更

--- G2A232Project/G2A232Project/Top.cs
+++ G2A232Project/G2A232Project/Top.cs
@@ -12,21 +12,22 @@ namespace G2A232Project
 {
     public partial class Top : Form
     {
+        //変数
+        private Menu _menu;
+
+
         public Top()
         {
             InitializeComponent();
         }
-        //変数
-        private Menu _menu;
-
         private void Form1_Load(object sender, EventArgs e)
         {
         }
         // 会員登録管理画面に遷移
-        private void button1_Click_1(object sender, EventArgs e)
+        private void Btn_start_Click_1(object sender, EventArgs e)
         {
 
-            // f2変数にForm2を格納して
+            // _menu変数にMenuを格納して
             _menu = new Menu();
             // 遷移したいForm.Show();
             _menu.Show();
@@ -34,7 +35,7 @@ namespace G2A232Project
         }
 
         //終了ボタン
-        private void button2_Click_1(object sender, EventArgs e)
+        private void Btn_end_Click_1(object sender, EventArgs e)
         {
             // システム終了の確認ダイアログ
             if (MessageBox.Show("終了してもよろしいですか?", "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
