9de42258dfe483e0e5c102100851b56ac1c2c309
YoshinoAoi <20217018-YoshinoAoi@hcs.ac.jp>
2022-12-23 12:26:54 +0900

    ～HCS code 規約に沿った変更～ using で使っていないものを削除 クラスコメント 追加 メソッドに"summary" 追加 メソッド名に変更(アンダーバー,小文字,大文字)

--- G2A232Project/G2A232Project/Top.cs
+++ G2A232Project/G2A232Project/Top.cs
@@ -1,41 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace G2A232Project
 {
+    // トップページクラス
     public partial class Top : Form
     {
         //変数
-        private Menu _menu;
-
+        private Menu menu;
 
         public Top()
         {
             InitializeComponent();
         }
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1Load(object sender, EventArgs e)
         {
         }
-        // 会員登録管理画面に遷移
-        private void Btn_start_Click_1(object sender, EventArgs e)
+        /// <summary>
+        /// 会員登録管理画面に遷移
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnStartClick(object sender, EventArgs e)
         {
 
             // _menu変数にMenuを格納して
-            _menu = new Menu();
+            menu = new Menu();
             // 遷移したいForm.Show();
-            _menu.Show();
+            menu.Show();
             this.Visible = false;
         }
 
-        //終了ボタン
-        private void Btn_end_Click_1(object sender, EventArgs e)
+        /// <summary>
+        /// 終了ボタン
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnEndClick(object sender, EventArgs e)
         {
             // システム終了の確認ダイアログ
             if (MessageBox.Show("終了してもよろしいですか?", "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
