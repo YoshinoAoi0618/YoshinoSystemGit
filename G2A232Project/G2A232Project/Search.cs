f466da6225da2117eceb6f7fb5ff2086ee225084
YoshinoAoi <20217018-YoshinoAoi@hcs.ac.jp>
2022-12-23 12:28:43 +0900

    ～HCS code 規約に沿った変更～ using で使っていないものを削除 クラスコメント 追加 メソッドに"summary" 追加 メソッド名に変更(アンダーバー,小文字,大文字) SQL文をConstで定数化

--- G2A232Project/G2A232Project/Search.cs
+++ G2A232Project/G2A232Project/Search.cs
@@ -1,38 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SQLite;
 
 namespace G2A232Project
 {
+    // 検索クラス
     public partial class Search : Form
     {
-        private Menu _menu;
+        // 変数
+        private Menu menu;
+
+        // インサート文を "const"で定数化
+        // SELECT文SQL
+        private const string SEARCH_SELECT= "SELECT * FROM MenberTable WHERE Id= ";
 
         public Search()
         {
             InitializeComponent();
         }
-        private void Search_Load(object sender, EventArgs e)
+        /// <summary>
+        /// フォームをロード中の処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SearchLoad(object sender, EventArgs e)
         {
             //偶数行の背景色を変更
-            searchDataGridView.AlternatingRowsDefaultCellStyle.BackColor = Color.SkyBlue;
+            MenberTableDataView.AlternatingRowsDefaultCellStyle.BackColor = Color.SkyBlue;
             //Gridの幅を列に揃える
-            searchDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            MenberTableDataView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             //行の高さをヘッダーとセルに合わせて自動調整
-            searchDataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            MenberTableDataView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
             //列の項目名を中央揃え
-            searchDataGridView.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            MenberTableDataView.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
         }
 
-        //検索
-        private void Btn_search_Click(object sender, EventArgs e)
+        /// <summary>
+        /// 検索
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSearchClick(object sender, EventArgs e)
         {
             try
             {
@@ -41,9 +51,9 @@ namespace G2A232Project
                     // DataTableを生成します。
                     var dataTable = new DataTable();
                     // SQLの実行
-                    var adapter = new SQLiteDataAdapter("SELECT * FROM t_product WHERE Id= "+ txt_search.Text, con);
+                    var adapter = new SQLiteDataAdapter(SEARCH_SELECT + txt_search.Text, con);
                     adapter.Fill(dataTable);
-                    searchDataGridView.DataSource = dataTable;
+                    MenberTableDataView.DataSource = dataTable;
                 }
             }
             //条件に合わなかったら、メッセージボックスにエラー内容を表示
@@ -54,11 +64,15 @@ namespace G2A232Project
             //データ入力後"検索"ボタンを押したらテキストボックスをリセットする
             txt_search.ResetText();
         }
-        //戻る
-        private void Btn_exit_Click(object sender, EventArgs e)
+        /// <summary>
+        /// 戻る
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnExitClick(object sender, EventArgs e)
         {
-            _menu = new Menu();
-            _menu.Show();
+            menu = new Menu();
+            menu.Show();
             this.Visible = false;
         }
     }
